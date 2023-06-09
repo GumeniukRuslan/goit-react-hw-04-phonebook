076611a23d09b7aa7aab628ffe5c32d89decec10
GumeniukRuslan <bishob1997@gmail.com>
2023-06-09 16:54:08 +0300

    made hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,52 @@
+import { useEffect, useState } from "react";
+import { ContactForm } from "./ContactFrom/ContactForm";
+import { ContactList } from "./ContactList/ContactList";
+import { Filter } from "./Filter/Filter";
+import { toast, Toaster } from "react-hot-toast";
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState();
+ 
+  useEffect(() => {
+    if (localStorage.getItem('contacts')) {
+      setContacts(JSON.parse(localStorage.getItem('contacts')))
+    };
+  }, [])
+
+  useEffect(() => {
+    contacts.length && localStorage.setItem("contacts", JSON.stringify(contacts))
+  }, [contacts])
+
+  const saveContact = (contact) => {
+    if (contacts.find(obj => obj.name.toLowerCase() === contact.name.toLowerCase())) {
+      const notify = () => toast.error('Contact is already in list.');
+      return notify()
+    }
+    setContacts((prevContacts) => [...prevContacts, contact])
+  };
+
+  const deleteContact = (evt) => setContacts(contacts.filter(contact => contact.id !== evt.target.closest('li').id));
+
+  const filterContacts = (evt) => setFilter(evt.target.value.trim());
+
+  
+
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm saveContact={saveContact} />
+
+        {contacts.length
+          ? <><h2>Contacts</h2>
+              <Filter filterValue={filterContacts} />
+              <ContactList deleteContact={deleteContact} contacts={filter
+                ? contacts.filter(obj => obj.name.toLowerCase().includes(filter.toLowerCase()))
+                : contacts} />
+            </>
+          : <p>Add some contacts</p>}
+        <Toaster toastOptions={{duration: 1500}} />
+      </div>
+    );
 };
